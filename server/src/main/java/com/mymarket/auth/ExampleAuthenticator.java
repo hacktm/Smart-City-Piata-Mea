e177cdaea9312b1ba68e92b8e25b785201de4397
Vlad Ilie <vlad.ilie@unifiedpost.com>
2014-10-18 11:46:20 +0300

    Initial server commit: basic hello app

---
+++ server/src/main/java/com/mymarket/auth/ExampleAuthenticator.java
@@ -0,0 +1,18 @@
+package com.mymarket.auth;
+
+import com.google.common.base.Optional;
+import com.mymarket.core.User;
+
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicCredentials;
+
+public class ExampleAuthenticator implements Authenticator<BasicCredentials, User> {
+    @Override
+    public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
+        if ("secret".equals(credentials.getPassword())) {
+            return Optional.of(new User(credentials.getUsername()));
+        }
+        return Optional.absent();
+    }
+}
---
+++ server/src/main/java/com/mymarket/resources/PersonResource.java
@@ -0,0 +1,37 @@
+package com.mymarket.resources;
+
+import com.google.common.base.Optional;
+import com.mymarket.core.Person;
+import com.mymarket.db.PersonDAO;
+import com.sun.jersey.api.NotFoundException;
+
+import io.dropwizard.hibernate.UnitOfWork;
+import io.dropwizard.jersey.params.LongParam;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/people/{personId}")
+@Produces(MediaType.APPLICATION_JSON)
+public class PersonResource {
+
+    private final PersonDAO peopleDAO;
+
+    public PersonResource(PersonDAO peopleDAO) {
+        this.peopleDAO = peopleDAO;
+    }
+
+    @GET
+    @UnitOfWork
+    public Person getPerson(@PathParam("personId") LongParam personId) {
+        final Optional<Person> person = peopleDAO.findById(personId.get());
+        if (!person.isPresent()) {
+            throw new NotFoundException("No such user.");
+        }
+        return person.get();
+    }
+
+}
