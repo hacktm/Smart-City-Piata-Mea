95e19ea0288297327f1f597201a428b5c5cf01a7
petru <petru.isfan@gmail.com>
2014-10-18 23:56:15 +0300

    Added background image

--- client/src/controllers/MarketCtrl.js
+++ client/src/controllers/MarketCtrl.js
@@ -6,7 +6,7 @@
         $scope.idMarket = $routeParams.idMarket;
         $scope.query = "";
 
-        $http.get("/api/market/1/products.jso")
+        $http.get("/api/market/1/products.json")
             .success(function(data) {
                 $scope.products = data;
             })
