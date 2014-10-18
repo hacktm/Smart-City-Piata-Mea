a661e3a9ad5643c51ac5f0c8726a7ae58242d49c
Andrei <andreyavram@yahoo.com>
2014-10-18 20:00:00 +0300

    Merge branch 'master' of https://github.com/hacktm/Smart-City-Piata-Mea
    Conflicts:
    	client/index.html

--- client/src/controllers/ProductEditCtrl.js
+++ client/src/controllers/ProductEditCtrl.js
@@ -1,3 +1,17 @@
 /**
  * Created by andrei on 18.10.2014.
  */
+'use strict';
+
+(function(app) {
+
+    var ProductEditCtrl = function($routeParams, $scope) {
+        var idMarket = $routeParams.idMarket;
+        var idProduct = $routeParams.idProduct;
+        console.log("products:", idMarket, idProduct);
+
+    };
+
+    app.controller("ProductEditCtrl", ProductEditCtrl);
+
+} (angular.module("mymarket")));
