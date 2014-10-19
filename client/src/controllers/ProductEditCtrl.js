62e657d04c3534332d9fff96b17585c3482e5678
petru <petru.isfan@gmail.com>
2014-10-19 16:22:19 +0300

    Update product

--- client/src/controllers/ProductEditCtrl.js
+++ client/src/controllers/ProductEditCtrl.js
@@ -5,9 +5,22 @@
 
 (function(app) {
 
-    var ProductEditCtrl = function($routeParams, $scope) {
+    var ProductEditCtrl = function($routeParams, $scope, $http, $location) {
         var idMarket = $routeParams.idMarket;
         var idProduct = $routeParams.idProduct;
+
+        $http.get("/api/product/" + idProduct)
+            .success(function(data) {
+                $scope.product = data;
+            })
+            .error(function() {
+                toastr.error("Produsul nu a putut fi citit de pe server");
+            });
+
+        $scope.updateProduct = function() {
+            toastr.success("Produsul a fost editat cu success");
+            $location.path("/market/" + idMarket);
+        };
     };
 
     app.controller("ProductEditCtrl", ProductEditCtrl);
