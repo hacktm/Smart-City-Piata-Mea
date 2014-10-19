71153042d1b4c4da9a0fa3b42724c1833936a905
Andrei <andreyavram@yahoo.com>
2014-10-19 10:57:14 +0300

    - Large UI update

--- client/src/directives/sidemenu.js
+++ client/src/directives/sidemenu.js
@@ -10,15 +10,25 @@
                 $http.get("/api/market.json")
                     .success(function(data) {
                         scope.markets = data;
+                        $.each(data, function(i,el){
+                            if(el.id == scope.idMarket)
+                                scope.activeMarket = el.location;
+                        });
+
+                        console.log("market",scope.activeMarket);
+                        $("#static_map").attr("src","https://maps.googleapis.com/maps/api/staticmap?center="+scope.activeMarket+"&zoom=16&size=250x180&maptype=roadmap&markers=size:big%7Ccolor:red%7C"+scope.activeMarket);
                     })
                     .error(function() {
                         toastr.error("Pietele nu au putut fi citite de pe server");
                     });
 
                 scope.isActive = function(id) {
+
                     return scope.idMarket == id;
                 }
+
             }
+
         }
     };
 
