94ef4a9de5ffebdfa5ba2f92f4ec19119e1eb5c5
petru <petru.isfan@gmail.com>
2014-10-19 11:58:05 +0300

    Added support for add product

--- client/src/directives/modals.js
+++ client/src/directives/modals.js
@@ -5,10 +5,22 @@
 
 (function(app) {
 
-    var modals = function() {
+    var modals = function($http) {
         return {
             restrict: "AE",
-            templateUrl: "views/directives/modals.html"
+            templateUrl: "views/directives/modals.html",
+            link: function(scope) {
+                scope.productAdd = function() {
+                    $http.post("/api/market" + scope.idMarket + "/product", scope.newProd)
+                        .success(function() {
+                            toastr.success("Produsul a fost salvat");
+                            $("#addproduct_modal").modal("close");
+                        })
+                        .error(function() {
+                            toastr.error("Produsul nu poate fi salvat.");
+                        })
+                }
+            }
         }
     };
 
